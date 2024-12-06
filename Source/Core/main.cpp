5786f2325c89d35eb1c423718e78fe69e928a5bf
ylongwang2782 <ylongwangi@qq.com>
2024-12-06 11:26:11 +0800

    add: 新增uid库
    - uid库可以直接读取设备uid

--- Source/Core/main.cpp
+++ Source/Core/main.cpp
@@ -1,14 +1,17 @@
 #include <stdint.h>
+
 #include <array>
 #include <cstdint>
 #include <cstdio>
 #include <string>
 
 #include "FreeRTOS.h"
-#include "bsp_log.h"
 #include "bsp_led.h"
+#include "bsp_log.h"
+#include "bsp_uid.h"
 #include "gd32f4xx.h"
 #include "task.h"
+
 
 void vTask1(void *pvParameters);
 void vTask2(void *pvParameters);
@@ -18,6 +21,8 @@ Serial usart1(usart1_config);
 LED led0(RCU_GPIOC, GPIOC, GPIO_PIN_6);
 
 int main(void) {
+    UIDReader &uid = UIDReader::getInstance();
+    usart1.data_send(uid.value.data(), uid.value.size());
     xTaskCreate(vTask1, "Task 1", 128, NULL, 1, NULL);
     xTaskCreate(vTask2, "Task 2", 128, NULL, 2, NULL);
     vTaskStartScheduler();

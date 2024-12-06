8adcd45173a13b6edb6ce5bf69e9f75cac6f91c0
ylongwang2782 <ylongwangi@qq.com>
2024-12-06 10:33:56 +0800

    Refactor： 暂时删除log功能和重定向功能，会导致异常

--- Source/BSP/src/bsp_log.cpp
+++ Source/BSP/src/bsp_log.cpp
@@ -1,9 +1,6 @@
 #include "bsp_log.h"
 
-#include "bsp_uart.h"
-#include "printf.h"
-
-
-// void _putchar(char character) {
+// void _putchar(char character) {    // 等待发送缓冲区为空
+//     while (RESET == usart_flag_get(USART1, USART_FLAG_TBE));
 //     usart_data_transmit(USART1, (uint8_t)character);
 // }

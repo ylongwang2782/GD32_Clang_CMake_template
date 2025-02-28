beb60e3af129532e5aba2c952a1bdf12477ba7e5
rainlong <ylongwangi@qq.com>
2025-02-28 13:36:55 +0800

    [Feature] (GPIO): 添加GPIO模式切换功能并实现引脚测试任务
    在GPIO类中新增`switch_to_input`和`switch_to_output`方法，支持动态切换引脚模式
    在`main.cpp`中新增`PinTest`类，用于测试62个引脚的输出功能
    添加`PinTestTask`任务，周期性切换所有测试引脚状态
    
    这些改动为硬件调试和引脚功能验证提供了便利。

--- Source/Core/main.cpp
+++ Source/Core/main.cpp
@@ -1,5 +1,8 @@
+#include <array>
+#include <cstddef>
 #include <cstdint>
 #include <cstdio>
+#include <vector>
 
 #include "TaskCPP.h"
 #include "TaskCpp.h"
@@ -17,13 +20,84 @@ extern "C" {
 }
 #endif
 
-
 UartConfig uart1Conf(usart1_info);
 UartConfig uart2Conf(usart2_info);
 UartConfig uart3Conf(uart3_info);
 Uart uart1(uart1Conf);
 Uart uart2(uart2Conf);
 Uart uart3(uart3Conf);
+
+class PinTest {
+   public:
+    struct PinConfig {
+        GPIO::Port port;
+        GPIO::Pin pin;
+    };
+
+    std::vector<GPIO> pins;
+
+    const std::array<PinConfig, 62> pinConfigs = {{
+        {GPIO::Port::A, GPIO::Pin::PIN_3},  {GPIO::Port::A, GPIO::Pin::PIN_4},
+        {GPIO::Port::A, GPIO::Pin::PIN_5},  {GPIO::Port::A, GPIO::Pin::PIN_6},
+        {GPIO::Port::A, GPIO::Pin::PIN_7},  {GPIO::Port::C, GPIO::Pin::PIN_4},
+        {GPIO::Port::C, GPIO::Pin::PIN_5},  {GPIO::Port::B, GPIO::Pin::PIN_0},
+        {GPIO::Port::B, GPIO::Pin::PIN_1},  {GPIO::Port::F, GPIO::Pin::PIN_11},
+        {GPIO::Port::F, GPIO::Pin::PIN_12}, {GPIO::Port::F, GPIO::Pin::PIN_13},
+        {GPIO::Port::F, GPIO::Pin::PIN_14}, {GPIO::Port::F, GPIO::Pin::PIN_15},
+        {GPIO::Port::G, GPIO::Pin::PIN_0},  {GPIO::Port::G, GPIO::Pin::PIN_1},
+        {GPIO::Port::E, GPIO::Pin::PIN_7},  {GPIO::Port::E, GPIO::Pin::PIN_8},
+        {GPIO::Port::E, GPIO::Pin::PIN_9},  {GPIO::Port::E, GPIO::Pin::PIN_10},
+        {GPIO::Port::E, GPIO::Pin::PIN_11}, {GPIO::Port::E, GPIO::Pin::PIN_12},
+        {GPIO::Port::E, GPIO::Pin::PIN_13}, {GPIO::Port::E, GPIO::Pin::PIN_14},
+        {GPIO::Port::E, GPIO::Pin::PIN_15}, {GPIO::Port::B, GPIO::Pin::PIN_10},
+        {GPIO::Port::B, GPIO::Pin::PIN_11}, {GPIO::Port::B, GPIO::Pin::PIN_12},
+        {GPIO::Port::B, GPIO::Pin::PIN_13}, {GPIO::Port::B, GPIO::Pin::PIN_14},
+        {GPIO::Port::B, GPIO::Pin::PIN_15}, {GPIO::Port::D, GPIO::Pin::PIN_8},
+        {GPIO::Port::D, GPIO::Pin::PIN_9},  {GPIO::Port::D, GPIO::Pin::PIN_10},
+        {GPIO::Port::D, GPIO::Pin::PIN_11}, {GPIO::Port::D, GPIO::Pin::PIN_12},
+        {GPIO::Port::D, GPIO::Pin::PIN_13}, {GPIO::Port::D, GPIO::Pin::PIN_14},
+        {GPIO::Port::D, GPIO::Pin::PIN_15}, {GPIO::Port::G, GPIO::Pin::PIN_2},
+        {GPIO::Port::G, GPIO::Pin::PIN_3},  {GPIO::Port::G, GPIO::Pin::PIN_4},
+        {GPIO::Port::G, GPIO::Pin::PIN_5},  {GPIO::Port::G, GPIO::Pin::PIN_6},
+        {GPIO::Port::G, GPIO::Pin::PIN_7},  {GPIO::Port::G, GPIO::Pin::PIN_8},
+        {GPIO::Port::C, GPIO::Pin::PIN_6},  {GPIO::Port::C, GPIO::Pin::PIN_7},
+        {GPIO::Port::C, GPIO::Pin::PIN_8},  {GPIO::Port::C, GPIO::Pin::PIN_9},
+        {GPIO::Port::A, GPIO::Pin::PIN_8},  {GPIO::Port::A, GPIO::Pin::PIN_9},
+        {GPIO::Port::A, GPIO::Pin::PIN_10}, {GPIO::Port::A, GPIO::Pin::PIN_11},
+        {GPIO::Port::A, GPIO::Pin::PIN_12}, {GPIO::Port::A, GPIO::Pin::PIN_15},
+        {GPIO::Port::C, GPIO::Pin::PIN_10}, {GPIO::Port::C, GPIO::Pin::PIN_11},
+        {GPIO::Port::D, GPIO::Pin::PIN_0},  {GPIO::Port::D, GPIO::Pin::PIN_1},
+        {GPIO::Port::D, GPIO::Pin::PIN_3},  {GPIO::Port::D, GPIO::Pin::PIN_4},
+    }};
+
+    PinTest() {}
+
+    void init() {
+        for (const auto &config : pinConfigs) {
+            pins.emplace_back(config.port, config.pin, GPIO::Mode::OUTPUT);
+        }
+    }
+};
+
+class PinTestTask : public TaskClassS<1024> {
+   public:
+    PinTestTask() : TaskClassS<1024>("LedBlinkTask", TaskPrio_Mid) {}
+
+    void task() override {
+        Logger &log = Logger::getInstance();
+        PinTest pinTest;
+        pinTest.init();
+
+        for (;;) {
+            log.d("Pin test.");
+            // 62 Pins Function Test
+            for (auto &pin : pinTest.pins) {
+                pin.toggle();
+            }
+            TaskBase::delay(500);
+        }
+    }
+};
 
 class LedBlinkTask : public TaskClassS<1024> {
    public:
@@ -49,7 +123,8 @@ class UsartDMATask : public TaskClassS<1024> {
         Logger &log = Logger::getInstance();
         for (;;) {
             // 等待 DMA 完成信号
-            if (xSemaphoreTake(uart3_info.dmaRxDoneSema, portMAX_DELAY) == pdPASS) {
+            if (xSemaphoreTake(uart3_info.dmaRxDoneSema, portMAX_DELAY) ==
+                pdPASS) {
                 log.d("Usart recv.");
                 uint8_t buffer[DMA_RX_BUFFER_SIZE];
                 uint16_t len =
@@ -81,6 +156,7 @@ class LogTask : public TaskClassS<1024> {
     }
 };
 
+PinTestTask pinTestTask;
 LedBlinkTask ledBlinkTask;
 UsartDMATask usartDMATask;
 LogTask logTask;
